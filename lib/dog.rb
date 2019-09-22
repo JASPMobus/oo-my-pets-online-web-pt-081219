c267a512d1106b8027afb5487893bcbce3923e5f
James Mobus <mobusj@live.com>
2019-09-22 17:19:11 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,13 @@
 class Dog
-  # code goes here
+  attr_reader :name 
+  attr_accessor :owner 
+  
+  @@all = []
+  
+  def initialize(name, owner)
+    @name = name
+    @owner = owner
+    
+    @@all << self
+  end
 end
