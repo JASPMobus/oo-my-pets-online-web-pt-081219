eba8b37fb82e6d40abc9e113df9eab7778454a32
James Mobus <mobusj@live.com>
2019-09-22 17:19:36 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,12 +1,13 @@
 class Dog
   attr_reader :name 
-  attr_accessor :owner 
+  attr_accessor :owner, :mood
   
   @@all = []
   
   def initialize(name, owner)
     @name = name
     @owner = owner
+    @mood = nervous
     
     @@all << self
   end
